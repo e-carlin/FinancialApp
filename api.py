e2468027c78841c1c8090366d0d96a24d7d416e1
Evan Carlin <evan@carlin.com>
2017-11-17 17:50:08 -0800

    Basic db read

--- api.py
+++ api.py
@@ -1,5 +1,25 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
+import os
+import psycopg2
+
+db_hostname = 'localhost'
+db_username = os.environ['APP_DB_USERNAME']
+db_password = os.environ['APP_DB_PASSWORD']
+db_name = os.environ['APP_DB_NAME']
+
+def doQuery( conn ) :
+    cur = conn.cursor()
+
+    cur.execute( "SELECT category_id, amount FROM transactions" )
+
+    for category, amount in cur.fetchall() :
+        print(category, amount)
+
+myConnection = psycopg2.connect( host=db_hostname, user=db_username, password=db_password, dbname=db_name )
+doQuery( myConnection )
+myConnection.close()
+
 
 app = Flask(__name__)
 api = Api(app)
