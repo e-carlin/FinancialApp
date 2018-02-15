78ff17769513cb60ac19dff441f60f1720266f7f
Evan Carlin <evan@carlin.com>
2018-02-15 10:21:20 -0800

    Connection info for ubuntu

--- banter_api/config.py
+++ banter_api/config.py
@@ -1,7 +1,7 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
-postgres_local_base = 'postgresql://banterapiuser:@localhost:5433/' # TODO: Change banterapiuser to be imported from settings
-database_name = os.getenv('BANTER_DB_NAME', 'banter')
+banter_db_localbase = 'postgresql://{}:{}@localhost/' # TODO: Change banterapiuser to be imported from settings
+banter_db_name = os.getenv('BANTER_DB_NAME', 'banter')
 
 
 class BaseConfig:
@@ -16,7 +16,9 @@ class DevelopmentConfig(BaseConfig):
     """Development configuration."""
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name
+    banter_db_user = os.getenv('BANTER_DB_USER')
+    bandter_db_password = os.getenv('BANTER_DB_PASS')
+    SQLALCHEMY_DATABASE_URI = banter_db_localbase.format(banter_db_user, bandter_db_password) + banter_db_name
     PLAID_CLIENT_ID = os.getenv('PLAID_CLIENT_ID')
     PLAID_SECRET_KEY = os.getenv('PLAID_SECRET_KEY')
     PLAID_PUBLIC_KEY = os.getenv('PLAID_PUBLIC_KEY')
@@ -27,7 +29,7 @@ class TestingConfig(BaseConfig):
     DEBUG = True
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
-    SQLALCHEMY_DATABASE_URI = postgres_local_base + database_name + '_test'
+    SQLALCHEMY_DATABASE_URI = banter_db_localbase + banter_db_name + '_test'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
